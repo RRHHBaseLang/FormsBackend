1ad14e6bafc7e1396aebf39fa24dce95b4d92b7f
AxelGod <148403077+Angeldadro@users.noreply.github.com>
2024-01-29 20:47:55 -0400

    handleDbErrors

--- Routers/SecPersona/app.py
+++ Routers/SecPersona/app.py
@@ -1,33 +1,49 @@
 from fastapi import HTTPException, APIRouter
 from adapters.db import session
 from Models.codigos import CodigosDb, testModel
-
+import functools
 router = APIRouter(prefix="/SecPersona", tags=["CodigosAcceso"])
 
 
+def handle_database_errors(func):
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except exc.OperationalError:
+            try:
+                session.rollback()
+            except exc.OperationalError as e:
+                raise HTTPException(
+                    status_code=500, detail=f"Error de reconexión: {str(e)}"
+                )
+            return func(self, *args, **kwargs)
+    return wrapper
+
+@handle_database_errors
 def is_test_started(user, test_id):
     try:
         comentarios = user.respComent
         print(comentarios)
         print(comentarios.get(test_id).get('status'))
-        if comentarios.get(test_id).get('status')==None:
+        if comentarios.get(test_id).get('status') == None:
             return False
-        print (comentarios.get(test_id).get('status')!="")
-        return comentarios.get(test_id).get('status')!=""
+        print(comentarios.get(test_id).get('status') != "")
+        return comentarios.get(test_id).get('status') != ""
     except:
         print("Error al leer los comentarios")
         return False
 
-
+@handle_database_errors
 def confirm_user_authorization(usable_id, test_id):
     user = session.query(CodigosDb).filter_by(_id=usable_id).first()
 
     if not user:
         raise HTTPException(status_code=401, detail="Credenciales inválidas")
     userForms = user.formIds
-    print (userForms)
+    print(userForms)
     print(str(test_id))
-    print (str(test_id) in userForms)
+    print(str(test_id) in userForms)
     if not str(test_id) in userForms:
         raise HTTPException(
             status_code=401, detail="No está autorizado a realizar esta prueba")
@@ -38,18 +54,18 @@ def confirm_user_authorization(usable_id, test_id):
 
     return user
 
-
+@handle_database_errors
 def confirm_user_authorization2(usable_id, test_id):
     user = session.query(CodigosDb).filter_by(_id=usable_id).first()
 
     if not user:
         raise HTTPException(status_code=401, detail="Credenciales inválidas")
-    
+
     if not test_id in user.formIds:
         raise HTTPException(
             status_code=401, detail="No está autorizado a realizar esta prueba")
-        
-    coments= user.respComent.get(test_id).get('status')
+
+    coments = user.respComent.get(test_id).get('status')
     if coments == 'Time over' or coments == 'finished' or coments == 'out of page':
         raise HTTPException(
             status_code=401, detail="Ya realizaste esta prueba"
@@ -57,8 +73,8 @@ def confirm_user_authorization2(usable_id, test_id):
     return user
 
 
-
 @router.post("/login")
+@handle_database_errors
 def login(usable_id, test_id):
     user = confirm_user_authorization(usable_id, test_id)
     lastcoments = user.respComent
@@ -73,7 +89,7 @@ def login(usable_id, test_id):
 
     return {"token": user._id}
 
-
+@handle_database_errors
 def update_test_status(usable_id, data, status):
     form_id, response_id = data.formId, data.responseId
     user = confirm_user_authorization(usable_id, form_id)
@@ -85,6 +101,7 @@ def update_test_status(usable_id, data, status):
         {CodigosDb.respComent: user.respComent})
     session.commit()
 
+@handle_database_errors
 def update_test_status2(usable_id, data, status):
     form_id, response_id = data.formId, data.responseId
     user = confirm_user_authorization2(usable_id, form_id)
@@ -97,22 +114,25 @@ def update_test_status2(usable_id, data, status):
     session.commit()
 
 
-
 @router.post("/start")
+@handle_database_errors
 def start_test(usable_id, data: testModel):
     update_test_status(usable_id, data, 'comenzado')
 
 
 @router.post("/over")
+@handle_database_errors
 def time_over(usable_id, data: testModel):
     update_test_status2(usable_id, data, 'Time over')
 
 
 @router.post("/out")
+@handle_database_errors
 def out_of_page(usable_id, data: testModel):
     update_test_status2(usable_id, data, 'out of page')
 
 
 @router.post("/finish")
+@handle_database_errors
 def finish_test(usable_id, data: testModel):
     update_test_status2(usable_id, data, 'finished')
--- Routers/typeform.py
+++ Routers/typeform.py
@@ -28,7 +28,7 @@ def handle_database_errors(func):
             return func(self, *args, **kwargs)
         except exc.OperationalError:
             try:
-                self.db.rollback()
+                session.rollback()
             except exc.OperationalError as e:
                 raise HTTPException(
                     status_code=500, detail=f"Error de reconexión: {str(e)}"
@@ -39,14 +39,14 @@ def handle_database_errors(func):
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="Security/Login")
 
-
+@handle_database_errors
 async def get_authenticated_user(token: str = Depends(oauth2_scheme)):
     auth = await autenticar_token(token)
     if not auth:
         raise HTTPException(status_code=401, detail="Credenciales incorrectas")
     return auth
 
-
+@handle_database_errors
 def _verify_token(token, decoded_token):
     if not decoded_token or not decoded_token.get('sub') or not decoded_token.get('exp'):
         raise HTTPException(
@@ -62,6 +62,7 @@ security_adapter = CRUDAdapter()
 
 
 @typeformRouter.post("/Crear")
+@handle_database_errors
 async def Crear(data: UserName, authenticated_user=Depends(get_authenticated_user), token: str = Depends(oauth2_scheme)):
     jwt_secret_key = os.getenv("JWT_SECRET_KEY") or "secret_key"
     jwt_algorithm = os.getenv("JWT_ALGORITHM") or "HS256"
@@ -86,20 +87,24 @@ async def Crear(data: UserName, authenticated_user=Depends(get_authenticated_use
     return(newdata)
 
 @typeformRouter.get("/Listar")
+@handle_database_errors
 async def Listar(authenticated_user=Depends(get_authenticated_user), token: str = Depends(oauth2_scheme)):
 
     return session.query(CodigosDb).all()
 
 @typeformRouter.get("/Listar/{id}")
+@handle_database_errors
 async def Listar(id, authenticated_user=Depends(get_authenticated_user), token: str = Depends(oauth2_scheme)):
     return session.query(CodigosDb).filter_by(_id=id).first()
 
 @typeformRouter.delete("/Eliminar/{id}")
+@handle_database_errors
 async def Eliminar(id, authenticated_user=Depends(get_authenticated_user), token: str = Depends(oauth2_scheme)):
     session.query(CodigosDb).filter_by(_id=id).delete()
     session.commit()
     
 @typeformRouter.put("/Refrescar/{id}")
+@handle_database_errors
 async def Refrescar(id, data: UserComentario, authenticated_user=Depends(get_authenticated_user)):
     lastcoments = session.query(CodigosDb).filter_by(_id=id).first().respComent
     newcoments = lastcoments[data.testid]= {}
