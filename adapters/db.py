30fe09d1154ae0f4434c779ba2ee09d8791ea318
AxelGod <159456033+RRHHBaseLang@users.noreply.github.com>
2024-02-08 11:36:48 -0400

    Update db.py

--- adapters/db.py
+++ adapters/db.py
@@ -5,11 +5,13 @@ from sqlalchemy.orm import sessionmaker
 import time
 
 
-
 def establish_connection():
     return create_engine(
-        f'{os.getenv("DATABASE_URL") or "postgresql://dbtype_user:zp5uFbyt5N7H28vO3d9PcAO7ng0R7n0T@dpg-cmp7l3fqd2ns738pa3h0-a.oregon-postgres.render.com/dbtype"}', 
+        f'{os.getenv("DATABASE_URL") or "postgresql://dbtype_user:zp5uFbyt5N7H28vO3d9PcAO7ng0R7n0T@dpg-cmp7l3fqd2ns738pa3h0-a.oregon-postgres.render.com/dbtype"}',
+        connect_args={'sslmode': 'allow'}
+
     )
+
 
 engine = establish_connection()
 Session = sessionmaker(bind=engine)
